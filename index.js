c838febbf86ed41cb375af0b26ed908b5394396f
Whzz <wanhuzz@foxmail.com>
2025-06-10 20:04:29 +0800

    'index'

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(express.static("public/uploads"));
+
+const router = require("./routes/index.js");
+
+app.use("/", router);
+
+app.listen(1437, () => {
+  console.log("http://localhost:1437");
+});
